c352634a2123c68d577b7e7239cb7bbb3d62996f
Claudio E. de Oliveira <claudioed.oliveira@gmail.com>
2020-02-09 11:23:48 -0300

    Svc draft

--- pkg/controller/apimock/deployment.go
+++ pkg/controller/apimock/deployment.go
@@ -1,47 +1,60 @@
 package apimock
 
 import (
+	"context"
 	"github.com/apirator/apirator/pkg/apis/apirator/v1alpha1"
 	"github.com/apirator/apirator/pkg/controller/k8s/util/labels"
 	"github.com/apirator/apirator/pkg/controller/k8s/util/owner"
 	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func deployment(mock *v1alpha1.APIMock) error {
-
-	var reps int32
-	reps = int32(1)
-
-	pt:= BuildPodTemplate(mock)
-
-	d:= v1.Deployment{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Deployment",
-			APIVersion: "apps/v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      mock.GetName(),
-			Namespace: mock.GetNamespace(),
-			Labels:    labels.LabelForAPIMock(mock),
-		},
-		Spec: v1.DeploymentSpec{
-			Replicas: &reps,
-			Selector: &metav1.LabelSelector{MatchLabels: labels.LabelForAPIMock(mock)},
-			Template: pt,
-			Strategy: v1.DeploymentStrategy{
-				Type: v1.RollingUpdateDeploymentStrategyType,
-				RollingUpdate: &v1.RollingUpdateDeployment{
-					MaxUnavailable: func(a intstr.IntOrString) *intstr.IntOrString { return &a }(intstr.FromInt(1)),
-					MaxSurge:       func(a intstr.IntOrString) *intstr.IntOrString { return &a }(intstr.FromInt(1)),
+func (r *ReconcileAPIMock) deployment(mock *v1alpha1.APIMock) error {
+	svcK8s := &v1.Deployment{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      mock.GetName(),
+		Namespace: mock.Namespace,
+	}, svcK8s)
+	if err != nil && errors.IsNotFound(err) {
+		var reps int32
+		reps = int32(1)
+		pt := BuildPodTemplate(mock)
+		d := v1.Deployment{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Deployment",
+				APIVersion: "apps/v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      mock.GetName(),
+				Namespace: mock.GetNamespace(),
+				Labels:    labels.LabelForAPIMock(mock),
+			},
+			Spec: v1.DeploymentSpec{
+				Replicas: &reps,
+				Selector: &metav1.LabelSelector{MatchLabels: labels.LabelForAPIMock(mock)},
+				Template: pt,
+				Strategy: v1.DeploymentStrategy{
+					Type: v1.RollingUpdateDeploymentStrategyType,
+					RollingUpdate: &v1.RollingUpdateDeployment{
+						MaxUnavailable: func(a intstr.IntOrString) *intstr.IntOrString { return &a }(intstr.FromInt(1)),
+						MaxSurge:       func(a intstr.IntOrString) *intstr.IntOrString { return &a }(intstr.FromInt(1)),
+					},
 				},
 			},
-		},
+		}
+		ref := owner.AsOwner(&mock.ObjectMeta)
+		d.SetOwnerReferences([]metav1.OwnerReference{ref})
+		err := r.client.Create(context.TODO(), d)
+		if err != nil {
+			log.Error(err, "Failed to create Deployment")
+			return err
+		}
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return err
 	}
-
-	owner.AddOwnerRefToObject(d, owner.AsOwner(&mock.ObjectMeta))
-
-
 	return nil
 }
--- pkg/controller/apimock/service.go
+++ pkg/controller/apimock/service.go
@@ -1,11 +1,14 @@
 package apimock
 
 import (
+	"context"
 	"github.com/apirator/apirator/pkg/apis/apirator/v1alpha1"
 	"github.com/apirator/apirator/pkg/controller/k8s/util/labels"
 	"github.com/apirator/apirator/pkg/controller/k8s/util/owner"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
@@ -13,38 +16,48 @@ const (
 	svcPortName = "mock-svc-port"
 )
 
-func service(mock *v1alpha1.APIMock) error {
-
+func (r *ReconcileAPIMock) service(mock *v1alpha1.APIMock) error {
 	svcPresent := mock.Spec.Port != 0
-
-	svcPort := v1.ServicePort{
-		Name:       svcPortName,
-		Protocol:   "http",
-		Port:       int32(mock.Spec.Port),
-		TargetPort: intstr.FromInt(mock.Spec.ContainerPort),
-	}
-
-	svc := &v1.Service{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Service",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      mock.GetName(),
-			Namespace: mock.GetNamespace(),
-		},
-		Spec: v1.ServiceSpec{
-			Selector: labels.LabelForAPIMock(mock),
-			Type:     v1.ServiceTypeLoadBalancer,
-			Ports:    []v1.ServicePort{svcPort},
-		},
-	}
-
-	owner.AddOwnerRefToObject(svc, owner.AsOwner(&mock.ObjectMeta))
-
 	if svcPresent {
-
+		svcK8s := &v1.Service{}
+		err := r.client.Get(context.TODO(), types.NamespacedName{
+			Name:      mock.GetName(),
+			Namespace: mock.Namespace,
+		}, svcK8s)
+		if err != nil && errors.IsNotFound(err) {
+			svcPort := v1.ServicePort{
+				Name:       svcPortName,
+				Protocol:   "http",
+				Port:       int32(mock.Spec.Port),
+				TargetPort: intstr.FromInt(mock.Spec.ContainerPort),
+			}
+			svc := &v1.Service{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "Service",
+					APIVersion: "v1",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      mock.GetName(),
+					Namespace: mock.GetNamespace(),
+				},
+				Spec: v1.ServiceSpec{
+					Selector: labels.LabelForAPIMock(mock),
+					Type:     v1.ServiceTypeLoadBalancer,
+					Ports:    []v1.ServicePort{svcPort},
+				},
+			}
+			owner.AddOwnerRefToObject(svc, owner.AsOwner(&mock.ObjectMeta))
+			err:= r.client.Create(context.TODO(),svc)
+			if err != nil{
+				log.Error(err, "Failed to create new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
+				return err
+			}
+		} else if err != nil {
+			log.Error(err, "Failed to get Service")
+			return err
+		}
+	}else {
+		log.Info("Service is not necessary")
 	}
-
 	return nil
 }
