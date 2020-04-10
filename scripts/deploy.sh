3d5b4d7cfbf34f1432b8075ea96f9ec52b4846ea
Tiago Angelo <tiago.angelo@sensedia.com>
2020-04-10 16:49:46 -0300

    Using version as container tag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ function run {
     eval $1
 }
 
-COMMIT_HASH=$(git log --oneline -1 --pretty="format:%h")
-run "operator-sdk build apirator/apirator:$COMMIT_HASH"
-run "docker push apirator/apirator:$COMMIT_HASH"
+VERSION=$(grep -o '".*"' ./version/version.go | sed 's/"//g')
+run "operator-sdk build apirator/apirator:$VERSION"
+run "docker push apirator/apirator:$VERSION"
 
 DEPLOYMENT=$(kubectl get deployment -l app=apirator -n oas -o=name)
 if [[ -z "$DEPLOYMENT" ]]
 then
-    run "cat deploy/operator.yaml | sed \"s/image:\ apirator\/apirator/image:\ apirator\/apirator:$COMMIT_HASH/\" | kubectl -n oas apply -f -"
+    run "cat deploy/operator.yaml | sed \"s/image:\ apirator\/apirator/image:\ apirator\/apirator:$VERSION/\" | kubectl -n oas apply -f -"
 else
-    run "kubectl set image deployment.extensions/apirator apirator=apirator/apirator:$COMMIT_HASH --record -n oas"
+    run "kubectl set image deployment.extensions/apirator apirator=apirator/apirator:$VERSION --record -n oas"
 fi
