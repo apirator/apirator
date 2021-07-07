f59a40e11ecdd566de69768d1bc75e90956950c5
Tiago Angelo <kurtis.angelo@gmail.com>
2021-07-07 07:08:30 -0300

    Get current APIMock state when reconciliation starts

--- controllers/apimock_controller.go
+++ controllers/apimock_controller.go
@@ -19,18 +19,16 @@ package controllers
 import (
 	"context"
 
-	"k8s.io/apimachinery/pkg/runtime"
+	"github.com/apirator/apirator/api/v1alpha1"
+	"github.com/apirator/apirator/internal/reconcile"
+	"k8s.io/apimachinery/pkg/api/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	apiratoriov1alpha1 "github.com/apirator/apirator/api/v1alpha1"
 )
 
 // APIMockReconciler reconciles a APIMock object
 type APIMockReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+	APIMockReader
+	APIMockAdapter
 }
 
 //+kubebuilder:rbac:groups=apirator.io,resources=apimocks,verbs=get;list;watch;create;update;patch;delete
@@ -39,24 +37,23 @@ type APIMockReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the APIMock object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *APIMockReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_, err := r.GetAPIMock(ctx, req.NamespacedName)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			return reconcile.DoNotRequeue()
+		}
+		return reconcile.RequeueOnErr(err)
+	}
 
 	// your logic here
 
-	return ctrl.Result{}, nil
+	return reconcile.DoNotRequeue()
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *APIMockReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&apiratoriov1alpha1.APIMock{}).
+		For(&v1alpha1.APIMock{}).
 		Complete(r)
 }
